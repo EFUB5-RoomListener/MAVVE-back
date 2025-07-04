dc0b6b69b8cd1670eb46ad98107853585350d6a9
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-07-04 20:19:34 +0900

    Chore: 프로젝트 기본 세팅

---
+++ mavve/src/test/java/com/efub/mavve/MavveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.mavve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
