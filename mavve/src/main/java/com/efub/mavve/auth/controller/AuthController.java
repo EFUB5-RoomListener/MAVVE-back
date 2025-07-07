9bf30a3831ee532984bf664c9cc14c0bdc4c505a
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-07-07 17:27:44 +0900

    Feat: reissue API 구현 #8

--- mavve/src/main/java/com/efub/mavve/auth/controller/AuthController.java
+++ mavve/src/main/java/com/efub/mavve/auth/controller/AuthController.java
@@ -4,7 +4,9 @@ import com.efub.mavve.auth.domain.User;
 import com.efub.mavve.auth.dto.request.KakaoCodeRequest;
 import com.efub.mavve.auth.service.AuthService;
 import com.efub.mavve.auth.service.jwt.CustomUserDetails;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -20,8 +22,14 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> loginOrRegister(@RequestBody KakaoCodeRequest request, HttpServletResponse response) {
+    public ResponseEntity<Void> loginOrRegister(@RequestBody @Valid KakaoCodeRequest request, HttpServletResponse response) {
         authService.loginOrRegisterUser(request, response);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<Void> reissue(@CookieValue(value = "refresh_token", required = false) String refreshToken, HttpServletResponse response) {
+        authService.reissue(refreshToken, response);
+        return ResponseEntity.created(null).build();
     }
 }
