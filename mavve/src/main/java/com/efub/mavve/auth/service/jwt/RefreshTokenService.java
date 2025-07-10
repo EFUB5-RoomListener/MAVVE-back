afdcae47e1d4897e188983c59dcc3da7297f3386
jettieb <bokjihee@naver.com>
2025-07-10 20:46:16 +0900

    Feat: 웹소켓 연결 및 cors 문제, redis bean 충돌 문제 해결 #18

--- mavve/src/main/java/com/efub/mavve/auth/service/jwt/RefreshTokenService.java
+++ mavve/src/main/java/com/efub/mavve/auth/service/jwt/RefreshTokenService.java
@@ -1,7 +1,7 @@
 package com.efub.mavve.auth.service.jwt;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -10,19 +10,19 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class RefreshTokenService {
     private static final String REFRESHPRIFIX = "RT";
-    private final RedisTemplate<String, String> redisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
 
     public void saveRefreshToken(String userId, String refreshToken, long durationMillis) {
-        redisTemplate.opsForValue()
+        stringRedisTemplate.opsForValue()
                 .set(REFRESHPRIFIX + userId, refreshToken, Duration.ofMillis(durationMillis));
     }
 
     public String getRefreshToken(String userId){
-        return redisTemplate.opsForValue().get(REFRESHPRIFIX + userId);
+        return stringRedisTemplate.opsForValue().get(REFRESHPRIFIX + userId);
     }
 
     public void deleteRefreshToken(String userId){
-        redisTemplate.delete(REFRESHPRIFIX + userId);
+        stringRedisTemplate.delete(REFRESHPRIFIX + userId);
     }
 
     public boolean existsRefreshToken(String userId, String refreshToken) {
