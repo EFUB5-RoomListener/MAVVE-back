b616c5ab5529f3602c659e6c5b3ae77d3b132e4a
Hong Jihyeong <154999028+topograp2@users.noreply.github.com>
2025-07-23 03:32:14 +0900

    Fix: Update deploy.sh

--- mavve/scripts/deploy.sh
+++ mavve/scripts/deploy.sh
@@ -1,17 +1,28 @@
-##!/bin/bash
+#!/bin/bash
 
-BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
+LOG_PATH=/home/ubuntu/deploy.log
+ERROR_LOG_PATH=/home/ubuntu/deploy_err.log
+
+BUILD_JAR=$(ls /home/ubuntu/app/mavve/build/libs/mavve-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
-
-echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
 DEPLOY_PATH=/home/ubuntu/app/
+
+echo ">>> ✅ 빌드 파일명: $JAR_NAME" >> $LOG_PATH
+
+echo ">>> 📦 이전 애플리케이션 중단 및 제거" >> $LOG_PATH
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -n "$CURRENT_PID" ]; then
+  echo ">>> 실행중인 프로세스 종료: $CURRENT_PID" >> $LOG_PATH
+  kill -15 $CURRENT_PID
+  sleep 5
+else
+  echo ">>> 실행중인 애플리케이션이 없습니다." >> $LOG_PATH
+fi
+
+echo ">>> 📂 JAR 복사 중..." >> $LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
-sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
-
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
-echo ">>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다" >> /home/ubuntu/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/deploy_err.log &
+echo ">>> 🚀 애플리케이션 실행: $DEPLOY_JAR" >> $LOG_PATH
+nohup java -jar $DEPLOY_JAR >> $LOG_PATH 2>> $ERROR_LOG_PATH &
