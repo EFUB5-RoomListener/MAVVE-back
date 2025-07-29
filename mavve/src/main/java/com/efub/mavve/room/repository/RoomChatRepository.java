42a9abe3069814198db5f604bf60eb797e8bcf59
jihee Bok <129582481+jettieb@users.noreply.github.com>
2025-07-30 05:17:39 +0900

    Revert "[Deploy] 시간순으로 이전 채팅 불러오기 반영"

--- mavve/src/main/java/com/efub/mavve/room/repository/RoomChatRepository.java
+++ mavve/src/main/java/com/efub/mavve/room/repository/RoomChatRepository.java
@@ -15,7 +15,7 @@ public interface RoomChatRepository extends JpaRepository<RoomChat, Long> {
             FROM RoomChat c 
             JOIN c.user u 
             WHERE c.room.roomId = :roomId 
-            AND (:lastChatId is NULL OR c.createdAt < :lastCreatedAt) 
+            AND (:lastChatId is NULL OR c.roomChatId < :lastChatId) 
             ORDER BY c.createdAt DESC
             """)
     List<ChatSummary> findChatsByRoomId(@Param("roomId") Long roomId, @Param("lastChatId") Long lastChatId, Pageable pageable);
