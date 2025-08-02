a221314b7ca24010a3c7aff83ccfb9f8aa29f555
9rin <129544574+9rin@users.noreply.github.com>
2025-08-03 01:47:34 +0900

    Revert "[Deploy] add-song 현재 노래 추가 로직 반영"

--- mavve/src/main/java/com/efub/mavve/room/service/websocket/RoomSongWebsocketService.java
+++ mavve/src/main/java/com/efub/mavve/room/service/websocket/RoomSongWebsocketService.java
@@ -9,7 +9,6 @@ import com.efub.mavve.room.payload.response.AddSongResponsePayload;
 import com.efub.mavve.room.payload.response.DeleteSongResponsePayload;
 import com.efub.mavve.room.payload.response.NextSongResponsePayload;
 import com.efub.mavve.room.payload.summary.SongRedis;
-import com.efub.mavve.room.payload.summary.SongSummary;
 import com.efub.mavve.room.service.redis.RoomSongRedisService;
 import com.efub.mavve.room.service.redis.RoomUserRedisService;
 import lombok.RequiredArgsConstructor;
@@ -31,7 +30,7 @@ import java.time.ZoneId;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class RoomSongService {
+public class RoomSongWebsocketService {
     private final RoomSongRedisService songRedisService;
     private final RoomUserRedisService userRedisService;
     private final PrincipalUtil principalUtil;
@@ -41,15 +40,7 @@ public class RoomSongService {
     private static final int BROADCAST_DELAY_SECONDS = 1;
 
     public AddSongResponsePayload addSong(Long roomCode, AddSongRequestPayload request) {
-        SongRedis song = SongSummary.toRedisPOJO(request.getSong());
-
-        // 현재 예약된 노래 없는 경우 - 현재 노래 예약 및 현재 노래 재생 브로드캐스트
-        if(!songRedisService.hasCurrentSong(roomCode)){
-            LocalDateTime startTime = startFirstSong(roomCode, song);
-            messagingTemplate.convertAndSend("/topic/rooms/" + roomCode + "/songs", NextSongResponsePayload.from(song, startTime));
-        }
-
-        SongRedis songAdd = songRedisService.addSong(roomCode, song);
+        SongRedis songAdd = songRedisService.addSong(roomCode, request.getSong());
         return AddSongResponsePayload.from(songAdd);
     }
 
@@ -85,16 +76,6 @@ public class RoomSongService {
         log.info("next song!");
         messagingTemplate.convertAndSend("/topic/rooms/" + roomCode + "/songs", NextSongResponsePayload.from(nextSong, startTime));
         scheduleNextSong(roomCode, nextSong);   // 다음 노래 스케쥴링
-    }
-
-    // 첫 번째 노래 재생 시작
-    public LocalDateTime startFirstSong(Long roomCode, SongRedis firstSong){
-        LocalDateTime startTime = LocalDateTime.now();
-
-        // 현재 노래 저장 및 다음 노래 스케쥴링
-        songRedisService.addCurrentSong(roomCode, firstSong, startTime);
-        scheduleNextSong(roomCode, firstSong);
-        return startTime;
     }
 
     // 노래 정보 받을 주소 구독 -> 사용자 정보 저장
